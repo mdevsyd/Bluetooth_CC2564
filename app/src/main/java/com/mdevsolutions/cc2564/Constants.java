04a756ffe632d81146edcff340c1f5e96ea92042
Michelle Quaglia <michelle@dosec.com.au>
2017-03-19 00:44:42 +1100

    Update BraodcastReceiver. Add enableHostDiscoverabilty method.

--- app/src/main/java/com/mdevsolutions/cc2564/Constants.java
+++ app/src/main/java/com/mdevsolutions/cc2564/Constants.java
@@ -20,6 +20,7 @@ public class Constants {
     public static final int REQUEST_ENABLE_BT = 100;
     public static final int REQUEST_CONNECT_DEVICE_SECURE = 200;
     public static final int REQUEST_CONNECT_DEVICE_INSECURE = 300;
+    public static final int REQUEST_HOST_DISCOVERABILITY = 400;
 
     // Messages sent from BT chat service handler
     public static final int MESSAGE_STATE_CHANGE = 1;
--- app/src/main/java/com/mdevsolutions/cc2564/DeviceListActivity.java
+++ app/src/main/java/com/mdevsolutions/cc2564/DeviceListActivity.java
@@ -42,7 +42,7 @@ public class DeviceListActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startDiscovery();
                 // make the current button disappear
-                //v.setVisibility(View.GONE);
+                v.setVisibility(View.GONE);
             }
         });
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -132,9 +132,15 @@ public class DeviceListActivity extends AppCompatActivity {
 
             // MAC hardware address of the device is the last 17 chars of the view
             String info = ((TextView) view).getText().toString();
-            String address = info.substring(info.length() - 17);
+            String address = info.substring(info.length()-17);
             String name = info.substring(0, info.length()-17);
             Log.d(Constants.DEBUG_TAG, "item was clicked " + address);
+
+            Toast.makeText(DeviceListActivity.this, name+", "+address,Toast.LENGTH_SHORT).show();
+
+            //enable local device discoverability
+            enableHostDiscoverability();
+
             //create intent including the hardware address
 
 //            Intent viewDeviceIntent = new Intent(DeviceListActivity.this, SelectedDeviceActivity.class);
@@ -146,38 +152,60 @@ public class DeviceListActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * Makes the host device discoverable to other BT devices for 2 mins (120 seconds)
+     */
+    private void enableHostDiscoverability() {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,120);
+        startActivityForResult(discoverableIntent, Constants.REQUEST_HOST_DISCOVERABILITY);
+
+    }
+
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             //get the action to check if device was found
             String action = intent.getAction();
-            if(BluetoothDevice.ACTION_FOUND.equals(action)){
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                //check if the device has already paired
-                if (device.getBondState() != BluetoothDevice.BOND_BONDED){
-                    mNewDevicesArrayAdapter.add(device.getName()+ "\n" +device.getAddress());
-                }
-            }
-            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action) && mBtAdapter.isDiscovering()){
 
-                if (mNewDevicesArrayAdapter.getCount()==0){
-                    //change display to show no devices were found
-                    String noDev = getResources().getText(R.string.no_discovered_device).toString();
-                    mNewDevicesArrayAdapter.add(noDev);
+            switch(action){
+                case BluetoothDevice.ACTION_FOUND:
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    //check if the device has already paired
+                    if (device.getBondState() != BluetoothDevice.BOND_BONDED){
+                        mNewDevicesArrayAdapter.add(device.getName()+ "\n" +device.getAddress());
+                    }
+                    break;
+                case BluetoothAdapter.ACTION_DISCOVERY_FINISHED:
+                    if (mNewDevicesArrayAdapter.getCount()==0){
+                        //change display to show no devices were found
+                        String noDev = getResources().getText(R.string.no_discovered_device).toString();
+                        mNewDevicesArrayAdapter.add(noDev);
+                        mBtAdapter.cancelDiscovery();
+                    }
+                    Button scanBtn = (Button)findViewById(R.id.button_scan);
+                    scanBtn.setVisibility(View.VISIBLE);
                     mBtAdapter.cancelDiscovery();
-                }
-                Button scanBtn = (Button)findViewById(R.id.button_scan);
-                scanBtn.setVisibility(View.VISIBLE);
-                mBtAdapter.cancelDiscovery();
-
+                    break;
+                case BluetoothAdapter.ACTION_STATE_CHANGED:
+                    if(mBtAdapter.getState() == BluetoothAdapter.STATE_OFF){
+                        enableBt();
+                    }
+                    break;
+                case BluetoothAdapter.ACTION_SCAN_MODE_CHANGED:
+                    int scanMode = intent.getParcelableExtra(BluetoothAdapter.EXTRA_SCAN_MODE);
+                    switch (scanMode){
+                        case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
+                            Toast.makeText(DeviceListActivity.this,R.string.connectable, Toast.LENGTH_SHORT).show();
+                            break;
+                        case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+                            Toast.makeText(DeviceListActivity.this,R.string.connectable_discoverable, Toast.LENGTH_SHORT).show();
+                            break;
+                        case BluetoothAdapter.SCAN_MODE_NONE:
+                            Toast.makeText(DeviceListActivity.this,R.string.discovery_error, Toast.LENGTH_SHORT).show();
+                            break;
+                    }
             }
-            //someone switches off Bluetooth
-            else if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)){
-                if(mBtAdapter.getState() == BluetoothAdapter.STATE_OFF){
-                    enableBt();
-                }
-            }
-
         }
     };
 
@@ -220,6 +248,12 @@ public class DeviceListActivity extends AppCompatActivity {
                 } else if (resultCode == RESULT_CANCELED) {
                     Toast.makeText(getApplicationContext(), "Bluetooth enabling failed. Exiting.", Toast.LENGTH_LONG).show();
                     finish();
+                }
+                break;
+            case Constants.REQUEST_HOST_DISCOVERABILITY:
+                if (resultCode == RESULT_CANCELED){
+                    Toast.makeText(DeviceListActivity.this, R.string.discovery_error, Toast.LENGTH_SHORT).show();
+
                 }
         }
     }
